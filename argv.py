cc154aa676a0bdce8e5b83c5e4503944ea8e6397
MaiA8 <mai.abbasg7@gmail.com>
2025-10-15 14:10:37 +0300

    Add files via upload

---
+++ argv.py
@@ -0,0 +1,12 @@
+from sys import argv
+#if len(argv) == 2:
+#    print(f"hello {argv[1]}")
+#else:
+#    print("hello world")   
+
+
+for i in argv[1:]:
+#    if i != 'argv.py':
+#        print(i)
+
+    print(i)
