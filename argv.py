596251cd6d00735a5993d6804875bb908ed10364
MaiA8 <mai.abbasg7@gmail.com>
2025-10-15 14:53:33 +0300

    Add files via upload

--- argv.py
+++ argv.py
@@ -1,12 +1,19 @@
-from sys import argv
+import sys
+#from sys import argv, exit
 #if len(argv) == 2:
 #    print(f"hello {argv[1]}")
 #else:
 #    print("hello world")   
 
 
-for i in argv[1:]:
+#for i in argv[1:]:
 #    if i != 'argv.py':
 #        print(i)
 
-    print(i)
+#    print(i)
+
+if len(sys.argv) != 2:
+    print("Missing command argument")
+    sys.exit(1)
+print(f"hello {sys.argv[1]}")
+sys.exit(0)
