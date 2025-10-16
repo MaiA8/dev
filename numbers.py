57e1145c9de1c5b55ae4d0f7108f5be61640a8d8
MaiA8 <mai.abbasg7@gmail.com>
2025-10-16 15:59:29 +0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,17 @@
+from sys import exit
+numbers = [4,6,2,8,1,0]
+#for i in range(len(numbers)):
+#    if numbers[i] == 0:
+#        print("found")
+#if 0 in numbers:
+#    print("found")
+#    exit(0)
+#print("Not Found")
+#exit(1)
+
+names = ['Mai', 'Medo', 'Mona', 'Mohamed', 'Lily']
+if 'Mai' in names:
+    print("found")
+    exit(0)
+print("not found")
+exit(1)
